26996a6ce52fa0ebc650988692a7130838b82065
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-05-31 12:55:04 +0200

    fix type-o

--- src/llm_chain_1.py
+++ src/llm_chain_1.py
@@ -21,4 +21,4 @@ result = model.invoke(messages)
 # print the result
 print(result)
 
-# content='ciao!' response_metadata={'token_usage': {'completion_tokens': 3, 'prompt_tokens': 20, 'total_tokens': 23}, 'model_name': 'gpt-4', 'system_fingerprint': None, 'finish_reason': 'stop', 'logprobs': None} id='run-a5f1fea8-b050-4eef-9f7d-a7fb813b6241-0' usage_metadata={'input_tokens': 20, 'output_tokens': 3, 'total_tokens': 23}
+# content='ciao!' response_metadata={'token_usage': {'completion_tokens': 3, 'prompt_tokens': 20, 'total_tokens': 23}, 'model_name': 'gpt-3.5-turbo', 'system_fingerprint': None, 'finish_reason': 'stop', 'logprobs': None} id='run-a5f1fea8-b050-4eef-9f7d-a7fb813b6241-0' usage_metadata={'input_tokens': 20, 'output_tokens': 3, 'total_tokens': 23}
